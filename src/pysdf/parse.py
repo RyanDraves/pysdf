79231674a3d0cd546726dbdf604da193b180cd1d
Ryan Draves <dravesr@umich.edu>
2020-06-27 15:03:13 -0400

    removed excess debugging

--- src/pysdf/parse.py
+++ src/pysdf/parse.py
@@ -91,7 +91,7 @@ def find_model_in_gazebo_dir(modelname):
           for modelnode in modelnodes:
             modelname_in_file = modelnode.attrib['name']
             if modelname_in_file not in find_model_in_gazebo_dir.cache:
-              print('Adding (name=%s, path=%s) to model cache' % (modelname_in_file, filename_path))
+              # print('Adding (name=%s, path=%s) to model cache' % (modelname_in_file, filename_path))
               find_model_in_gazebo_dir.cache[modelname_in_file] = filename_path
     # print(find_model_in_gazebo_dir.cache)
   if '/' in modelname:  # path-based
